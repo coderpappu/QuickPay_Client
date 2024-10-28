b1f0c32d0732695a53acea992e7a352a325af456
Pappu Dey <pappudeyprogrammer@gmail.com>
2024-10-28 15:45:08 +0600

    employee file system updated

--- src/components/employee/AssetCard.jsx
+++ src/components/employee/AssetCard.jsx
@@ -24,71 +24,73 @@ const AssetCard = () => {
   const initialValues = { file: null }; // Initialize file value
 
   return (
-    <div className="w-[49%] relative p-4 bg-white dark:bg-dark-card rounded-md">
-      <h1 className="text-xl font-medium mb-4 dark:text-dark-heading-color">
-        Asset Upload
-      </h1>
-      {docsList?.data?.map((doc) => (
-        <div key={doc.id}>
-          <Formik
-            enableReinitialize
-            initialValues={initialValues}
-            // Add your validationSchema here
-            onSubmit={async (values, { setSubmitting }) => {
-              const formData = modifyPayload({
-                companyName: "xceed",
-                employeeName: "pappu",
-                file: values.file,
-              });
+    <div className="w-full   mx-5 mt-5 mb-2 rounded-mde flex flex-wrap justify-between ">
+      <div className="w-[49%] relative p-4 bg-white dark:bg-dark-card rounded-md">
+        <h1 className="text-xl font-medium mb-4 dark:text-dark-heading-color">
+          Asset Upload
+        </h1>
+        {docsList?.data?.map((doc) => (
+          <div key={doc.id}>
+            <Formik
+              enableReinitialize
+              initialValues={initialValues}
+              // Add your validationSchema here
+              onSubmit={async (values, { setSubmitting }) => {
+                const formData = modifyPayload({
+                  companyName: "codex",
+                  employeeName: "pappu",
+                  file: values.file,
+                });
 
-              try {
-                const response = await uploadImage(formData).unwrap();
-                console.log("Upload response:", response);
-              } catch (error) {
-                console.error("Upload error:", error.data || error.message);
-              } finally {
-                setSubmitting(false);
-              }
-            }}
-          >
-            {({ isSubmitting, setFieldValue }) => (
-              <Form>
-                <div className="mb-4">
-                  <label
-                    htmlFor="file"
-                    className="block text-sm font-medium text-gray-700"
+                try {
+                  const response = await uploadImage(formData).unwrap();
+                  console.log("Upload response:", response);
+                } catch (error) {
+                  console.error("Upload error:", error.data || error.message);
+                } finally {
+                  setSubmitting(false);
+                }
+              }}
+            >
+              {({ isSubmitting, setFieldValue }) => (
+                <Form className="flex flex-wrap items-center gap-4">
+                  <div className="mb-4">
+                    <label
+                      htmlFor="file"
+                      className="block text-sm font-medium dark:text-dark-text-color"
+                    >
+                      {doc?.name}
+                    </label>
+                    <input
+                      type="file"
+                      name="file"
+                      id="file"
+                      onChange={(event) => {
+                        const file = event.currentTarget.files[0];
+                        setFieldValue("file", file); // Set file in Formik's values
+                      }}
+                      className="mt-1 block w-[250px] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#3686FF] focus:border-[#3686FF] sm:text-sm"
+                    />
+                    <ErrorMessage
+                      name="file"
+                      component="div"
+                      className="text-red-500 text-sm mt-1"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="px-4 py-2 bg-blue-600 text-white rounded-md"
                   >
-                    {doc?.name}
-                  </label>
-                  <input
-                    type="file"
-                    name="file"
-                    id="file"
-                    onChange={(event) => {
-                      const file = event.currentTarget.files[0];
-                      setFieldValue("file", file); // Set file in Formik's values
-                    }}
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#3686FF] focus:border-[#3686FF] sm:text-sm"
-                  />
-                  <ErrorMessage
-                    name="file"
-                    component="div"
-                    className="text-red-500 text-sm mt-1"
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="px-4 py-2 bg-blue-600 text-white rounded-md"
-                >
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      ))}
+                    Save
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
